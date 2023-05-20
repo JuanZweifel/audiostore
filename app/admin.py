2656ff0911b405c5b2af5e06d8329e2422037f72
JuanZweifel <jua.borquez@duocuc.cl>
2023-05-20 16:50:11 -0400

    Modelos de categoria y marca

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from app.models import Categoria
+from app.models import Marca
 
 # Register your models here.
+
+class admCategoria(admin.ModelAdmin):
+    list_display=["id_cat","nom_cat"]
+    list_editable=["nom_cat"]
+    class meta:
+        model=Categoria
+
+class admMarca(admin.ModelAdmin):
+    list_display=["id_marca","nom_marca"]
+    list_editable=["nom_marca"]
+    class meta:
+        model=Marca
+
+admin.site.register(Categoria, admCategoria)
+admin.site.register(Marca, admMarca)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    id_cat=models.AutoField(primary_key=True, null=False)
+    nom_cat=models.CharField(max_length=50, null=False)
+
+class Marca(models.Model):
+    id_marca=models.AutoField(primary_key=True, null=False)
+    nom_marca=models.CharField(max_length=50, null=False)
