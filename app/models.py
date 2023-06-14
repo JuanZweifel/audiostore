b026051d751099a2b94b7a13ff28cc27d47ebb9a
JuanZweifel <jua.borquez@duocuc.cl>
2023-06-13 20:18:36 -0400

    Uso de plantilla (Falta views y url)

--- app/models.py
+++ app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    id_cat=models.AutoField(primary_key=True, null=False)
+    nom_cat=models.CharField(max_length=50, null=False)
+
+class Marca(models.Model):
+    id_marca=models.AutoField(primary_key=True, null=False)
+    nom_marca=models.CharField(max_length=50, null=False)
+
+    def __str__(self):
+        return self.nom_marca
+    
+class Producto(models.Model):
+    id_producto = models.AutoField(primary_key= True, null=False)
+    nom_producto = models.CharField(max_length=50, null=False)
+    precio = models.IntegerField(null=False)
+    descripcion = models.TextField(max_length=300, null=False)
+    stock = models.IntegerField(null=False)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT)
--- app/views.py
+++ app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Producto
 
 # Create your views here.
+def index(request):
+    producto=Producto.objects.all()
+    context= {
+        "producto":producto
+    }
+    return render(request, 'app/index_principal.html', context)
+
+def categoria(request):
+    producto=Producto.objects.all()
+    context= {
+        "producto":producto
+    }
+    return render(request, 'app/categoria.html', context)
+
+def detalle_producto(request, id):
+    producto_det = get_object_or_404(Producto, id_producto = id)
+    return render(request, "app/producto.html", {'producto_det':producto_det})
