c641d794db592958acb89936816e4b1bf194a6ef
Aldo Arroyo <ald.arroyo@duocuc.cl>
2023-06-27 14:51:26 -0400

    datatable
    Implementación de datatable en productos falta hacer arreglos al estilo

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include,path
-from .views import index, categoria, detalle_producto, index_admin, crearproducto, modificarproducto
-
+from .views import modificarproducto, eliminarproducto,productos
+from .views import index, categoria, detalle_producto, index_admin, crearproducto, lista_productos
 urlpatterns = [
     path('', index, name='index'),
     path('categoria', categoria, name='categoria'),
@@ -9,4 +9,8 @@ urlpatterns = [
     path('index_admin', index_admin, name='index_admin'),
     path('crearproductos', crearproducto, name='crearproductos'),
     path('modificarproducto/<id>',modificarproducto, name="modificarproducto"),
+    path('eliminarproducto/<id>',eliminarproducto, name="eliminarproducto"),
+    path('productos',productos, name="productos"),
+    path('lista_productos/',lista_productos, name='lista_productos'),
+    
 ]
--- app/views.py
+++ app/views.py
@@ -2,8 +2,9 @@ import os
 from django.conf import settings
 from django.shortcuts import render, get_object_or_404, redirect
 from django.forms import formset_factory, modelformset_factory
-from .models import Producto, ImagenProducto
+from .models import Producto, ImagenProducto, Marca, Categoria
 from .forms import frmProducto, frmImagen, ImageFormSet
+from django.http.response import JsonResponse
 
 # Create your views here.
 def index(request):
@@ -42,7 +43,7 @@ def crearproducto(request):
                     imagen.producto = producto
                     imagen.save()
         
-        return redirect(to="index") 
+        return redirect(to="index_admin") 
     
     else:
         form = frmProducto()
@@ -58,32 +59,40 @@ def crearproducto(request):
 def modificarproducto(request, id):
     producto = get_object_or_404(Producto,id_producto=id)
     form = frmProducto(instance=producto)
-    ImageFormSet = modelformset_factory(ImagenProducto, form=frmImagen, extra=3, can_delete=True, can_delete_extra=True)
+    #Verificar cuantas imagenes tiene asociada una noticia para poder indicar el numero de formularios extra del formset
+    numero_imagenes = ImagenProducto.objects.filter(producto=producto).count()
+    numero_imagenes_formset = 3-numero_imagenes
+    ImageFormSet = modelformset_factory(ImagenProducto, form=frmImagen, extra=numero_imagenes_formset, can_delete=True, can_delete_extra=True)
     formset = ImageFormSet(queryset=ImagenProducto.objects.filter(producto=producto))
+
 
     if request.method == "POST":
         form = frmProducto(request.POST, instance=producto)
         formset = ImageFormSet(request.POST, request.FILES, queryset=ImagenProducto.objects.filter(producto=producto))
         
-        
         if form.is_valid() and formset.is_valid():
             producto = form.save()
-            
+            formset.save(commit=False)
             for formm in formset:                
                 if  formm.cleaned_data.get('imagen'):
-                    elemento_id  = formm.cleaned_data['id_imagen']
-                    if elemento_id:
-                        # Eliminar el elemento
-                        imagenn = ImagenProducto.objects.get(imagen=elemento_id)
-                        if imagenn:
-                            ruta_imagen = imagenn.imagen.url
-                            ruta_media = os.path.join(settings.MEDIA_ROOT)
-                            nombre = os.path.basename(ruta_imagen)
-                            ruta_final = ruta_media + '/Productos/' + nombre
-                        
-                            if os.path.exists(ruta_final):
-                                os.remove(ruta_final)
-                    #asdsa
+                    imagen_id = formm.instance.id_imagen
+                    if imagen_id:
+                        # Eliminar la imagen existente
+                        try:
+                            imagenn = ImagenProducto.objects.get(id_imagen=imagen_id)
+                            img = formm.instance.imagen
+                            if imagenn.imagen != img:
+                                ruta_imagen = imagenn.imagen.url
+                                ruta_media = os.path.join(settings.MEDIA_ROOT)
+                                nombre = os.path.basename(ruta_imagen)
+                                ruta_final = ruta_media + '/Productos/' + nombre
+                            
+                                if os.path.exists(ruta_final):
+                                    os.remove(ruta_final)
+                            
+                        except ImagenProducto.DoesNotExist:
+                            pass 
+
                     imagen = formm.save(commit=False)
                     imagen.producto = producto
                     imagen.save()
@@ -102,10 +111,57 @@ def modificarproducto(request, id):
                         if os.path.exists(ruta_final):
                             os.remove(ruta_final)
                             
-            return redirect('index')
+            return redirect('index_admin')
 
     context = {
         'form': form,
         'formset': formset,
     }
     return render(request, 'app/admin/adminProductoModificar.html', context)
+
+
+def eliminarproducto(request,id):
+    producto=get_object_or_404(Producto,id_producto=id)
+
+    contexto={
+
+        "producto":producto,
+    }
+
+    if request.method=="POST":
+        img = ImagenProducto.objects.filter(producto=producto)
+        
+        if img:
+            for imagenn in img:
+                    ruta_imagen = imagenn.imagen.url
+                    ruta_media = os.path.join(settings.MEDIA_ROOT)
+                    nombre = os.path.basename(ruta_imagen)
+                    ruta_final = ruta_media + '/Productos/' + nombre
+                
+                    if os.path.exists(ruta_final):
+                        os.remove(ruta_final)
+        producto.delete()
+        return redirect(to="index_admin")
+
+
+    return render(request,"app/admin/adminProductoEliminar.html",contexto)
+
+
+def productos(request):
+
+    
+    return render(request,"app/admin/adminProducto.html")
+
+def lista_productos(_request):
+    productos = list(Producto.objects.values())
+
+    for producto in productos:
+        idmarca= producto['marca_id']
+        idcategoria= producto['categoria_id']
+        
+        producto['marca_id']=Marca.objects.get(id_marca=idmarca).nom_marca
+        producto['categoria_id']=Categoria.objects.get(id_cat=idcategoria).nom_cat
+    
+    data={'productos':productos}
+    
+    return JsonResponse(data)
